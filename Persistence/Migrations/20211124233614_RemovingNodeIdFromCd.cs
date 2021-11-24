bc91f17247f0f823f6d5428500ea2232844bc373
gutsdream <angus.hankinson@gmail.com>
2021-11-25 12:42:36 +1300

    migration

---
+++ Persistence/Migrations/20211124233614_RemovingNodeIdFromCd.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class RemovingNodeIdFromCd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NodeId",
+                table: "ConnectionDetails");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDateUtc",
+                table: "NodeSnapshots",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedDateUtc",
+                table: "NodeSnapshots");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "NodeId",
+                table: "ConnectionDetails",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
