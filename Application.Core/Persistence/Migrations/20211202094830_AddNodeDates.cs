b0886ccff18e7ee5a78ae20a42e8ef03986744da
gutsdream <angus.hankinson@gmail.com>
2021-12-02 23:21:50 +1300

    migrations

---
+++ Application.Core/Persistence/Migrations/20211202094830_AddNodeDates.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddNodeDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastBackupDateUtc",
+                table: "Nodes",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastCleanedDateUtc",
+                table: "Nodes",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastSnapshotDateUtc",
+                table: "Nodes",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastBackupDateUtc",
+                table: "Nodes");
+
+            migrationBuilder.DropColumn(
+                name: "LastCleanedDateUtc",
+                table: "Nodes");
+
+            migrationBuilder.DropColumn(
+                name: "LastSnapshotDateUtc",
+                table: "Nodes");
+        }
+    }
+}
